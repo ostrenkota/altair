08b0d1d4d84116ea759e29ad2bccb80d6dc35d08
Таисия Андреева <tandreeva@solit-clouds.com>
2024-03-07 22:01:55 +0100

    fix: main screen overflow handled

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,15 @@ import {useState} from "react";
 export const HomePage = () => {
     const [input, setInput] = useState('');
     return (
-        <Box bgColor="#f3f3f3" height="100%" px="30px" pt="40px">
-            <Box mb="30px">
-                <Input bgColor="lightgoldenrodyellow" placeholder="search" value={input} onChange={e => setInput(e.target.value)}/>
-            </Box>
-            <Box>
-                <ClientsRecordsList filter={input}/>
+        <Box height="100%" overflow="auto">
+            <Box height="fit-content" bgColor="#f3f3f3" px="30px" py="40px">
+                <Box mb="30px">
+                    <Input bgColor="lightgoldenrodyellow" placeholder="search" value={input}
+                           onChange={e => setInput(e.target.value)}/>
+                </Box>
+                <Box>
+                    <ClientsRecordsList filter={input}/>
+                </Box>
             </Box>
         </Box>
     )
