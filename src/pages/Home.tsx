bd41cc2084ef9c1f051cd69e526451713d01b24e
Таисия Андреева <tandreeva@solit-clouds.com>
2024-03-07 22:05:27 +0100

    fix: added min height for the background wrapper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ export const HomePage = () => {
     const [input, setInput] = useState('');
     return (
         <Box height="100%" overflow="auto">
-            <Box height="fit-content" bgColor="#f3f3f3" px="30px" py="40px">
+            <Box minH="100%" height="fit-content" bgColor="#f3f3f3" px="30px" py="40px">
                 <Box mb="30px">
                     <Input bgColor="lightgoldenrodyellow" placeholder="search" value={input}
                            onChange={e => setInput(e.target.value)}/>
